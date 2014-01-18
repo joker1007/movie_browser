54bb372f94381e0f0b8f864a65df963ac2b33264
joker1007 <kakyoin.hierophant@gmail.com>
2014-01-18 19:18:35 +0900

    Fix typo

--- src/main/resources/db/migration/V20140102015210__Create_fileinfos_table.sql
+++ src/main/resources/db/migration/V20140102015210__Create_fileinfos_table.sql
@@ -3,10 +3,10 @@ create table fileinfos (
   id bigserial not null primary key,
   md5 varchar(512) not null unique,
   fullpath varchar(512) not null,
-  relativePath varchar(512) not null,
+  relative_path varchar(512) not null,
   basename varchar(255) not null,
   filesize bigint not null,
-  targetId bigint not null,
+  target_Id bigint not null,
   created_at timestamp not null,
   updated_at timestamp
 )
--- src/main/resources/db/migration/V20140106220928__Create_targets_table.sql
+++ src/main/resources/db/migration/V20140106220928__Create_targets_table.sql
@@ -2,7 +2,7 @@
 create table targets (
   id bigserial not null primary key,
   fullpath varchar(512) not null,
-  asPath varchar(512),
+  as_path varchar(512),
   last_updated_at timestamp,
   created_at timestamp not null,
   updated_at timestamp
