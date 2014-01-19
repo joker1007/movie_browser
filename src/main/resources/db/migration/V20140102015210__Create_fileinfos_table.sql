f0ab0345af00acd92b48140aa595a59c37e5d3f3
joker1007 <kakyoin.hierophant@gmail.com>
2014-01-19 13:11:04 +0900

    Make fileinfos.md5  not unique

--- src/main/resources/db/migration/V20140102015210__Create_fileinfos_table.sql
+++ src/main/resources/db/migration/V20140102015210__Create_fileinfos_table.sql
@@ -1,7 +1,7 @@
 -- For H2 Database
 create table fileinfos (
   id bigserial not null primary key,
-  md5 varchar(512) not null unique,
+  md5 varchar(512) not null,
   fullpath varchar(512) not null,
   relative_path varchar(512) not null,
   basename varchar(255) not null,
